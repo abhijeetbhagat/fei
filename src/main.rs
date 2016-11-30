0bc8edc97f2cffffb77af14dfd3a01d413df0939
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2016-11-29 17:25:09 -0800

    Add RRQ/WRQ packet creation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
+extern crate rand;
 use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
 use std::io;
+
+#[ repr(u16) ]
+enum PacketType{
+    RRQ = 1,
+    WRQ,
+    DATA,
+    ACK,
+    ERROR
+}
+
+static NETASCII: &'static str = "netascii";
+static OCTET: &'static str = "octet";
+
+fn create_rrq_wrq_packet(p_type: PacketType, file_name: String, mode : &'static str)->Vec<u8>{
+    let mut v = Vec::with_capacity(2+file_name.len()+1+mode.len()+1);
+    v.push(0);
+    v.push(p_type as u8);
+    v.extend(file_name.as_bytes());
+    v.push(0);//zero terminator
+    v.extend(mode.as_bytes());
+    v.push(0);//zero terminator
+    v
+}
+
+fn create_data_packet()->Vec<u8>{
+
+}
 
 fn recv() -> Result<(), io::Error> { 
     // Define the local connection information 
     let ip = Ipv4Addr::new(127, 0, 0, 1); 
-    let connection = SocketAddrV4::new(ip, 9991);
+    let connection = SocketAddrV4::new(ip, 69);
 
     // Bind the socket
     let socket = try!(UdpSocket::bind(connection));
@@ -25,13 +53,14 @@ fn recv() -> Result<(), io::Error> {
 fn send() -> Result<(), io::Error> { 
     // Define the local connection (to send the data from) 
     let ip = Ipv4Addr::new(127, 0, 0, 1); 
+    //TODO generate port # using rand
     let connection = SocketAddrV4::new(ip, 9992);
 
     // Bind the socket
     let socket = try!(UdpSocket::bind(connection));
 
     // Define the remote connection (to send the data to)
-    let connection2 = SocketAddrV4::new(ip, 9991);
+    let connection2 = SocketAddrV4::new(ip, 69);
 
     // Send data via the socket
     let buf = &[0x01, 0x02, 0x03];
