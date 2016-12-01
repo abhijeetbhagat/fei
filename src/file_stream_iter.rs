70b06b424fccc9d0061890cf8417b5ea1e6f45d7
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2016-12-01 12:38:18 -0800

    Move FileStream to a separate file

---
+++ src/file_stream_iter.rs
@@ -0,0 +1,49 @@
+use std::io::{BufReader, Read, Error};
+use std::fs::File;
+
+pub struct FileStream<Read>{
+    file_name : String,
+    start : u64,
+    end:u64,
+    buf:[u8;512],
+    reader : BufReader<Read>
+}
+
+impl FileStream<File>{ //std::fs::File because we are talking concrete implementation
+    pub fn new(file_name:String)->Option<Self>{
+        let f = match File::open(file_name.clone()){
+            Ok(handle) => Some(handle),
+            Err(msg) => None
+
+        };
+        if f.is_some(){
+            let mut fs = FileStream{
+                file_name:file_name,
+                start: 0,
+                end: 0,
+                reader : BufReader::with_capacity(512, f.unwrap()),
+                buf : [0;512]
+            };
+            Some(fs)
+        }
+        else{
+            println!("cant open file");
+            None
+        }
+    }
+}
+
+impl Iterator for FileStream<File>{
+    type Item = ([u8;512], usize);
+    fn next(&mut self)->Option<Self::Item>{
+        let num_bytes_read = self.reader.read(&mut self.buf);
+        println!("{:?}", num_bytes_read);
+        let mut arr = [0;512];
+        let mut i = 0usize;
+        for c in self.buf.iter(){
+            arr[i] = *c;
+            i += 1; 
+        }
+        Some((arr, num_bytes_read.unwrap()))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+mod file_stream_iter;
 extern crate rand;
 use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
-use std::io::{BufReader, Read, Error};
-use std::fs::File;
+use file_stream_iter::FileStream;
+use std::io::Error;
 
 #[ repr(u8) ]
 enum PacketType{
@@ -26,52 +27,6 @@ fn create_rrq_wrq_packet(p_type: PacketType, file_name: String, mode : &'static 
     v
 }
 
-struct FileStream<Read>{
-    file_name : String,
-    start : u64,
-    end:u64,
-    buf:[u8;512],
-    reader : BufReader<Read>
-}
-
-impl FileStream<std::fs::File>{ //std::fs::File because we are talking concrete implementation
-    fn new(file_name:String)->Option<Self>{
-        let f = match std::fs::File::open(file_name.clone()){
-            Ok(handle) => Some(handle),
-            Err(msg) => None
-
-        };
-        if f.is_some(){
-            let mut fs = FileStream{
-                file_name:file_name,
-                start: 0,
-                end: 0,
-                reader : BufReader::with_capacity(512, f.unwrap()),
-                buf : [0;512]
-            };
-            Some(fs)
-        }
-        else{
-            println!("cant open file");
-            None
-        }
-    }
-}
-
-impl Iterator for FileStream<std::fs::File>{
-    type Item = ([u8;512], usize);
-    fn next(&mut self)->Option<Self::Item>{
-        let num_bytes_read = self.reader.read(&mut self.buf);
-        println!("{:?}", num_bytes_read);
-        let mut arr = [0;512];
-        let mut i = 0usize;
-        for c in self.buf.iter(){
-            arr[i] = *c;
-            i += 1; 
-        }
-        Some((arr, num_bytes_read.unwrap()))
-    }
-}
 
 /*
 fn create_data_packet()->Vec<u8>{
@@ -153,6 +108,11 @@ fn send() -> Result<(), Error> {
 
     println!("sender recvd {:?}", buf);
     Ok(()) 
+}
+
+fn send_over_wire(buf : &[u8]){
+    
+
 }
 
 fn main() {
